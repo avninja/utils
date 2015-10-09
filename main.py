941e1fc17e8750683eedd10e7e409792dbaa352b
Kirk Griffes <kgriffes@gmail.com>
2015-10-09 17:38:21 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #import udp
 from processhandler import *
-import socket, time
+import socket, time, sys
 from multiprocessing import Process, Queue, freeze_support
 
-def server_start(port, event_q):
+def server_start(event_q):
+	port = 33333
 	# Create socket and bind to address
 	UDPSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	UDPSock.bind(('', port))
@@ -20,12 +21,12 @@ def server_start(port, event_q):
 if __name__ == "__main__":
 	freeze_support()
 	event_q = Queue()
-	p = Process(target=server_start, name='udpserver', args=(33333, event_q,))
-	p.start()
-
-	#functions = [server.server_start]
-	#p = ProcessHandler(functions)
+	#p = Process(target=server_start, name='udpserver', args=(33333, event_q,))
 	#p.start()
+
+	functions = [server_start]
+	p = ProcessHandler(functions)
+	p.start()
 
 	try:
 		while True:
@@ -34,5 +35,5 @@ if __name__ == "__main__":
 				print event_q.get()
 			time.sleep(0.01)
 	except(KeyboardInterrupt):
-		p.join()
+		p.close()
 		sys.exit(0)
